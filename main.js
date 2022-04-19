9559b06c8ba68e89e6226c7e7e7d7cd1863258a3
Sariah Campopiano <scampopiano925@gmail.com>
2022-04-18 21:17:46 -0500

    Added javascript to practice mutating the DOM's html & css

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Working");
+// const mainButton = document.getElementById("main-btn");
+// console.log(mainButton);
+
+const rootElement = document.querySelector("#root");
+
+rootElement.innerHTML = `<button id="main-btn">This is a button</button>`;
+
+rootElement.innerHTML += `<button id="main-btn">This is a button</button>`;
+rootElement.innerHTML += `<button id="main-btn">This is a button</button>`;
+rootElement.innerHTML += `<button id="main-btn">This is a button</button>`;
+rootElement.innerHTML += `<button id="main-btn">This is a button</button>`;
+
+
+const bodyElement = document.querySelector("body");
+
+bodyElement.style.backgroundColor = "#316896";
